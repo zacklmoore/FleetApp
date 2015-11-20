4d2caef944b6e268852de30bdb864c4fc7172dd1
shrop21 <shrop21@gmail.com>
2015-11-20 17:56:46 -0600

    View Controllers
    Skeleton App. Segue between list view and map does not work yet.

---
+++ MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  FleetApp
+//
+//  Created by Jake Shropshire on 11/20/15.
+//  Copyright © 2015 CS407. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UITableViewController {
+
+    
+    
+}
---
+++ fleet_mockup/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  FleetApp
+//
+//  Created by Jake Shropshire on 11/18/15.
+//  Copyright © 2015 CS407. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
