53f274b8e9e2426dfc7948d5543176f81fb8eeb9
Afbcary <afbcary@gmail.com>
2015-11-22 17:00:22 -0600

    Added files for Additional viewControllers

---
+++ fleet_mockup/ChatViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ChatViewController.swift
+//  FleetApp
+//
+//  Created by Emily Greinwald on 11/22/15.
+//  Copyright © 2015 CS407. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ fleet_mockup/JoinViewController.swift
@@ -0,0 +1,21 @@
+//
+//  JoinViewController.swift
+//  FleetApp
+//
+//  Created by Emily Greinwald on 11/22/15.
+//  Copyright © 2015 CS407. All rights reserved.
+//
+import UIKit
+
+class JoinViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
