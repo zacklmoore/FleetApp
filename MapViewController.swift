8e827be3d9f9a6bc368c92a7b5a6032dedcd1d7a
Zackary Moore <zacklmoore@gmail.com>
2015-11-22 16:23:13 -0600

    Fixed the Map View.

--- MapViewController.swift
+++ MapViewController.swift
@@ -9,8 +9,18 @@
 import UIKit
 import MapKit
 
-class MapViewController: UITableViewController {
+class MapViewController: UIViewController {
 
+    @IBOutlet weak var MapView: MKMapView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
 }
